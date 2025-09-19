b3fa28a2771ae8b304bee71b268caf445efbb7fa
Elian Marcel Carvalho Gomes <elian.gomes@amazoninf.com.br>
2025-09-19 17:59:42 -0300

    teste commit

--- aula2.py
+++ aula2.py
@@ -1,5 +1,6 @@
 #Função print exibe algo na tela, recebe vários argumentos
 #usa-se virgulas, texto usa-se entre aspas simples ou duplas
+#Recebe argumentos
 print(12, 34, sep= "-") #sep adiciona um separador, aspas duplas
 print(30, 204, sep= ' e ')#sep adiciona um separador, aspas simples
 print('Texto entre aspas simples')
