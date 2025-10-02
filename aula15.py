283627f909208606ba8077c1896bcb075d729278
Elian Marcel Carvalho Gomes <elian.gomes@amazoninf.com.br>
2025-10-02 15:56:53 -0300

    Adiconando comentarios sobre input e mudança de variavel

--- aula15.py
+++ aula15.py
@@ -3,10 +3,11 @@
 
 #numero_1 = int(input('Digite um número: '))
 #numero_2 = int(input('Digite outro número: ')) não recomendado fazer a conversão na mesma linha
-#
+#Input sempre é string
 numero_1 = input('Digite um número: ')
 numero_2 = input('Digite outro número: ')
-
+#sempre realizar a mudança da variavel apos a entrada
+#Em outra linha de coditgo
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
