83ef037bf0e8a45eb31f01a1082dd577d01d95a7
Elian Marcel Carvalho Gomes <elian.gomes@amazoninf.com.br>
2025-10-05 11:52:54 -0300

    refazendo atividade aula32.1

---
+++ aula32.1.py
@@ -0,0 +1,42 @@
+# Numero_digitado = input("Digite um número: ")
+# try:
+#     Numero_digitado_int = int(Numero_digitado)
+#     Num_par = Numero_digitado_int %2 == 0
+#     if Num_par:
+#         print(f"O número inteiro digitado {Numero_digitado_int} é Par")
+#     else:
+#         print(f"O número inteiro digitado {Numero_digitado_int} é impar")
+# except:
+#     print(f"O valor digitado {Numero_digitado} não é interiro")
+
+# Horario_digitado = input("Digite o horário: ")
+
+# try:
+#     Horario_digitado_int = int(Horario_digitado)
+
+#     if Horario_digitado_int <= 23:
+#         if Horario_digitado_int <= 11:
+#             print("Bom dia")
+#         elif (Horario_digitado_int >= 12) and (Horario_digitado_int <= 17):
+#             print("Boa tarde")
+#         elif (Horario_digitado_int >=18) and (Horario_digitado_int <=23):
+#             print("Boa noite")
+#     else:
+#         print("Horários corretos vão de 0 à 23 horas")
+
+# except:
+#     print("Não é inteiro") 
+
+
+# Nome_usuario = input("Digite seu nome: ")
+# Nome_usuario_tamanho = len(Nome_usuario)
+
+# # print(Nome_usuario_tamanho)
+
+# if Nome_usuario_tamanho <= 4:
+#     print(f"{Nome_usuario}, seu nome é pequeno e tem {Nome_usuario_tamanho} letras")
+# elif (Nome_usuario_tamanho >= 5) and (Nome_usuario_tamanho <=6):
+#     print(f"{Nome_usuario}, seu nome é normal e tem {Nome_usuario_tamanho} letras ")
+# else:
+#     print(f"{Nome_usuario}, seu nome é grande e tem {Nome_usuario_tamanho} letras ")
+
