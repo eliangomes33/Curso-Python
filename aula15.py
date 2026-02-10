0cbe5999ff80816065b1fb60552051a439853f9a
Dex057 <86822933+Dex057@users.noreply.github.com>
2026-02-10 13:19:33 -0300

    adcionado aula41,42,43

--- aula15.py
+++ aula15.py
@@ -3,7 +3,7 @@
 
 #numero_1 = int(input('Digite um número: '))
 #numero_2 = int(input('Digite outro número: ')) não recomendado fazer a conversão na mesma linha
-#Input sempre é string
+#Input sempre é stringk
 numero_1 = input('Digite um número: ')
 numero_2 = input('Digite outro número: ')
 #sempre realizar a mudança da variavel apos a entrada
---
+++ aula47.py
@@ -0,0 +1,7 @@
+print("JOGO: ADVINHE A PALAVRA")
+
+palavra = 'Eligol'
+
+while True:
+    palavra = 'Eligol'
+    letra_digitada = input('Digite uma letra:')
