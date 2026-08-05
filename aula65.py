9dd9f7e5417f8dc9c3caabf20244336251334028
Elian Gomes <eliangomes3312@gmail.com>
2026-08-05 10:39:38 -0300

    aula 65 a 67

---
+++ aula65.py
@@ -0,0 +1,14 @@
+"""
+Introdução as funções (def)
+Funçoes são trechos de código usados para
+replicar detetrminada ação ao longo do código
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor especifico.
+Por padrão,funções Python retornam None (nada)
+
+"""
+
+def saudacao(nome):
+    print(f'Olá, {nome}!')
+
+saudacao('Elian')
